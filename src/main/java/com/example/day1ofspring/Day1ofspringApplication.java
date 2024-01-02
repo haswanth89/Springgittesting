7f5433f607f507eaf40076541abc6dfe4de4bc83
Haswanth <haswanth89@gmail.com>
2024-01-02 12:31:50 -0500

    Initial commit

---
+++ src/main/java/com/example/day1ofspring/Day1ofspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.day1ofspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1ofspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day1ofspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day1ofspring/Day1ofspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day1ofspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1ofspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
