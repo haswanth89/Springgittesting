4fdd047fc67c47c67cc66c68c62f127963c49c55
Haswanth <haswanth89@gmail.com>
2024-01-04 13:27:34 -0500

    added controller and template

---
+++ src/main/java/com/example/day1ofspring/controller.java
@@ -0,0 +1,14 @@
+package com.example.day1ofspring;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class controller {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello, World!");
+        return "hello";
+    }
+}
+
